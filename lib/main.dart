89092612a845ae777a3ffcfe2a954b74ed0f280f
teshlya@mail.ru <teshlya@mail.ru>
2020-02-25 09:19:24 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:carpenter/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
